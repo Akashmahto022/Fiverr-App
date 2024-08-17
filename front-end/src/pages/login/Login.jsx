05c2e889b7f69dc50533954bbf3478f2234d262b
Akashmahto022 <akashmahto1169@gmail.com>
2024-08-17 12:10:40 +0530

    bulding login page

--- front-end/src/pages/login/Login.jsx
+++ front-end/src/pages/login/Login.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './Login.scss';
+import React from "react";
+import "./Login.scss";
 
 const Login = () => {
   return (
-    <div className='login'>
-      Login
-    </div>
-  )
-}
+    <div className="login">
+      <form action="">
 
-export default Login
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input type="text" name="username" placeholder="username" />
+        <label htmlFor="">Password</label>
+        <input type="password" name="password" placeholder="password" />
+        <button>Login</button>
+        
+      </form>
+    </div>
+  );
+};
+
+export default Login;
